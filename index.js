aade0b3018e0dffbd8dfd279b2ab31de311a6ff5
Jon Nordby <jononor@gmail.com>
2012-07-08 00:06:05 +0200

    Move the /process and /files handlers to toplevel of server.js
    Also rename start() to createServer() and let it take a portNumber.
    Preparation for making these things public API.

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-var server = require("./server");
+var gegl = require("./server");
 
-server.start();
+gegl.createServer(8888);
--- server.js
+++ server.js
@@ -1,27 +1,70 @@
 
 /* Server module */
 
-var static = require('node-static');
 var encoder = new require('node-html-encoder').Encoder('entity')
+var static = require('node-static');
 
 var http = require("http");
 var url = require('url')
 var querystring = require("querystring");
 
-var processor = require('./processor')
+var processor = require("./processor");
 
+// FIXME: make these configurable from outside of module
+var files_directory = './public'
+var files_url_pathname = '/files'
+var files_url = 'http://localhost:8888' + files_url_pathname
 
-function start() {
+var file_server = new(static.Server)(files_directory);
 
-  var file_server = new(static.Server)(files_directory);
+function serveFile(request, response) {
 
-  var files_directory = './public'
-  var files_url_pathname = '/files'
-  var files_url = 'http://localhost:8888' + files_url_pathname
+    var pathname = url.parse(request.url).pathname;
+    var file_name = pathname.replace(files_url_pathname, '');
 
-  // TODO: Unhardcode. Make parameters of the HTTP request on a "/process" path
-  // TODO: Implement a demo "application" that allows users to
-  // specify the graph XML and output filename, and hit "Process" to get the output
+    console.log("Serving file: " + file_name);
+
+    file_server.serveFile('/' + file_name, 200, {}, request, response, function (err, result) {
+        if (err) {
+            response.writeHead(err.status, err.headers);
+            response.end();
+        }
+    })
+}
+
+function process(request, response) {
+    console.log("Processing request: " + url)
+
+    var postData;
+
+    request.addListener("data", function(postDataChunk) {
+        postData += postDataChunk;
+    });
+
+    request.addListener("end", function() {
+
+        var args = querystring.parse(postData)
+        console.log(args)
+        var output_file = files_directory + '/' + args.outputFile
+
+        processor.process(args.undefinedgraphData, output_file, function(output_path, err) {
+            if (err) {
+                response.write(err)
+            } else {
+                response.writeHead(200, {"Content-Type": "text/html"});
+                response.write("File written to " + output_path + "<br>");
+                path = output_path.replace(files_directory, files_url)
+                response.write(path + "<br>");
+                response.write('<img src=' +  path + '/>');
+            }
+            response.end();
+        });
+
+    })
+}
+
+function createServer(portNumber) {
+
   var example_xml = require("fs").readFileSync('tests/data/example.xml')
 
   function onRequest(request, response) {
@@ -32,20 +75,13 @@ function start() {
     request.setEncoding("utf8");
     
     if (pathname.indexOf('/files') === 0) {
+        serveFile(request, response)
 
-        var file_name = pathname.replace(files_url_pathname, files_directory)
-        console.log("Serving file: " + file_name);
-        file_server.serveFile('/' + file_name, 200, {}, request, response, function (err, result) {
-            if (err) {
-                response.writeHead(err.status, err.headers);
-                response.end();
-            }
-        })
+    } else if (pathname == "/process") {
+        process(request, response)
     
-    } else if (pathname == "/favicon.ico") {
-        console.log("Ignoring facicon request")
-
     } else if (pathname == "/") {
+        // Serve demo application
 
         response.writeHead(200, {"Content-Type": "text/html"});    
 
@@ -68,49 +104,16 @@ function start() {
         response.write(body)
         response.end();
 
-    } else if (pathname == "/process") {
-
-        console.log("Processing request: " + url)
-
-        var postData;
-        request.addListener("data", function(postDataChunk) {
-            postData += postDataChunk;
-        });
-
-        request.addListener("end", function() {
-
-            //console.log(encoder.htmlDecode(decodeURIComponent(postData).replace(/([+])/g, ' ')) + '\n')
-
-            var args = querystring.parse(postData)
-            console.log(args)
-            var output_file = files_directory + '/' + args.outputFile
-
-            processor.process(args.undefinedgraphData, output_file, function(output_path, err) {
-                if (err) {
-                    response.write(err)
-                } else {
-                    response.writeHead(200, {"Content-Type": "text/html"});
-                    response.write("File written to " + output_path + "<br>");
-                    path = output_path.replace(files_directory, files_url)
-                    response.write(path + "<br>");
-                    response.write('<img src=' +  path + '/>');
-                }
-                response.end();
-            });
-
-        })
     } else {
         response.writeHead(404, {"Content-Type": "text/plain"})
         response.write("Page not found")
         response.end();
     }
 
-
-
   }
 
-  http.createServer(onRequest).listen(8888);
+  http.createServer(onRequest).listen(portNumber);
   console.log("Server has started.");
 }
 
-exports.start = start;
+exports.createServer = createServer;
