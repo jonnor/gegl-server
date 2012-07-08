2964ca11a71816ab96a4bab255809d3f8310ed9b
Jon Nordby <jononor@gmail.com>
2012-07-08 03:51:05 +0200

    package.json: Admit involvement, set license to MIT

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 #!/usr/bin/env node
+
+/* This file is part of gegl-server
+ * License: MIT, http://www.opensource.org/licenses/mit-license.html
+ * Copyright 2012, Jon Nordby <jononor@gmail.com (http://jonnor.com)
+ */
+
 // gegl-server executable
 
 gegl_server = require('../gegl-server')
