5b29a270b9b13e7798c2053fc3adf3a95830f106
Jon Nordby <jononor@gmail.com>
2012-08-23 14:39:12 +0200

    Fix demo application not showing processed image
    The jQuery "intelligent" guessing of response type caused parsing to fail.
    Specify the dataType manually instead. Also add a error handler and remove jQuery example code.

--- public/demo-clientside.js
+++ public/demo-clientside.js
@@ -1,16 +1,16 @@
 $(document).ready(function(){
-    $("a").click(function(event){
-        alert("As you can see, the link no longer took you to jquery.com");
-        event.preventDefault();
-    });
-    
+
     $("#graphForm").submit(function(event) {  
         
-        $.post("/process", $("#graphForm").serialize(),
-            function(data) {
-                console.log("output of POST" + data);
-                $("#previewImage").attr("src", data);
-            });
+        var xhr = $.post("/process", $("#graphForm").serialize(), null, 'text')
+        .success(function(data) {
+            console.log("output of POST" + data);
+            $("#previewImage").attr("src", data);
+        })
+        .error(function(jqXHR) {
+            console.log("Error occurred during processing")
+            console.log(jqXHR)
+        });
         event.preventDefault();
         return false;
     });
